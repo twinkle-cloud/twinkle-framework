d6ec3a2fcc3f58dfceabf557545d6f1f9e6e6238
Administrator <Administrator@D5SQV2K2ASI1CSU>
2019-07-13 15:32:33 +0800

    调整pom 依赖；新增twinkle-demo 用于开发调试

---
+++ twinkle-demo/src/main/java/com/twinkle/framework/demo/Application.java
@@ -0,0 +1,11 @@
+package com.twinkle.framework.demo;
+
+/**
+ * demo Application
+ *
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
